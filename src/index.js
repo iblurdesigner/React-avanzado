05f48eb59383736df0d61c9bef7aace8a4cfb802
iblurdesigner <nevomen@gmail.com>
2019-12-06 10:02:54 -0500

    Linter, extensiones y deploy con Now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import  React from 'react'
+import React from 'react'
 import ReactDom from 'react-dom'
 
-ReactDom.render(<h1>'Hola Platzi seguimos  en elcusro'</h1>, document.getElementById('app'))
+ReactDom.render(<h1>'Hola Platzi seguimos  en el curso'</h1>, document.getElementById('app'))
