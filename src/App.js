a219cd16cff9393bd902a6f52de504fbe174ef4f
iblurdesigner <nevomen@gmail.com>
2019-12-06 13:21:25 -0500

    Creando el PhotoCard y usando react-icons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
 
+import { ListOfPhotoCard } from './components/ListOfPhotoCard'
+
 export const App = () => (
   <div>
     <GlobalStyles />
     <ListOfCategories />
+    <ListOfPhotoCard />
   </div>
 )
