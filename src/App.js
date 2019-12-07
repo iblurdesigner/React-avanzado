1558b76bf63c02aeff1dbdab2ed081368f1c7ce6
iblurdesigner <nevomen@gmail.com>
2019-12-07 12:55:34 -0500

    de SVG a components - SVGR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
-
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCard />
   </div>
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+    width:150px;
+    margin-left: 15px;
+    margin-top: 15px;
+`
