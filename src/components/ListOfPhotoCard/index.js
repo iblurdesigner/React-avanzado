29de2523808901b1bb7b05c0a9197d475249dd1e
iblurdesigner <nevomen@gmail.com>
2020-02-14 16:09:10 -0500

    conf graphql y apollo, obteniendo imagenes reales

--- src/components/ListOfPhotoCard/index.js
+++ src/components/ListOfPhotoCard/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCard = () => {
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+const withPhotos = graphql(gql`
+query getPhotos{
+  photos{
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`)
+
+const ListOfPhotoCardComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
       {
-        [1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)
+        photos.map(photo => <PhotoCard key={photo.id} {...photo} />)
       }
     </ul>
   )
 }
+
+export const ListOfPhotoCard = withPhotos(ListOfPhotoCardComponent)
