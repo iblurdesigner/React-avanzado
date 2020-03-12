47d344898c07ce69b3eedbaa32c3418641da6c5c
iblurdesigner <nevomen@gmail.com>
2020-03-12 13:18:44 -0500

    Agregando un NavBar

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { Link as LinkRouter } from '@reach/router'
+
+export const Nav = styled.nav`
+    align-items: center;
+    background: #fcfcfc;
+    border-top: 1px solid #e0e0e0;
+    bottom: 0;
+    display: flex;
+    height: 50px;
+    justify-content: space-around;
+    left: 0;
+    margin: 0 auto;
+    position: fixed;
+    right: 0;
+    width: 100%;
+    z-index: 1000;
+    font-size: 32px;
+`
+
+export const Link = styled(LinkRouter)`
+    align-items: center;
+    color: #888;
+    display: inline-flex;
+    height: 100%;
+    justify-content: center;
+    text-decoration: none;
+    width: 100%;
+`
