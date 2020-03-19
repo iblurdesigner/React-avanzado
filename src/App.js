f5df7331bef3a8dba4dea8601b5112d3b9d57cb2
iblurdesigner <nevomen@gmail.com>
2020-03-19 15:18:47 -0500

    Intro React Context

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Router } from '@reach/router'
-
-const UserLogged = ({ children }) => {
-  return children({ isAuth: true })
-}
+import Context from './Context'
 
 export const App = () => {
   return (
@@ -26,20 +23,20 @@ export const App = () => {
         <Detail path='/detail/:detailId' />
       </Router>
 
-      <UserLogged>
+      <Context.Consumer>
         {
           ({ isAuth }) =>
             isAuth
               ? <Router>
                 <Favs path='/favs' />
                 <User path='/user' />
-                </Router>
+              </Router>
               : <Router>
                 <NotRegisteredUser path='/favs' />
                 <NotRegisteredUser path='/user' />
-                </Router>
+              </Router>
         }
-      </UserLogged>
+      </Context.Consumer>
       <NavBar />
     </div>
   )
