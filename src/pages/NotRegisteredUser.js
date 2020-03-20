e93baecdce8d32655850e50494503ccf2d5723b6
iblurdesigner <nevomen@gmail.com>
2020-03-20 13:13:45 -0500

    UserForm y Hook useInputValue

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Context from '../Context'
+import { UserForm } from '../components/UserForm'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {
-      ({ isAuth, activateAuth }) => {
-        return (
-          <form onSubmit={activateAuth}>
-            <button>Iniciar sesión</button>
-          </form>
-        )
+      ({ activateAuth }) => {
+        return <UserForm onSubmit={activateAuth} />
       }
     }
   </Context.Consumer>
