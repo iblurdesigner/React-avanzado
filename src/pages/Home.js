9814c6d26ca572580eda2803a6f78344cc917b51
iblurdesigner <nevomen@gmail.com>
2020-03-24 19:05:30 -0500

    react Helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
+import { Layout } from '../components/Layout'
 
 export const Home = ({ id }) => {
   return (
-    <>
+    <Layout title='Tu app de fotos de mascotas' subtitle='Con Petgram puedes encontrar fotos de animales domésticos muy bonitos'>
+      <title>Petgram - </title>
+      <meta name='description' content='' />
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
-    </>
+    </Layout>
   )
 }
