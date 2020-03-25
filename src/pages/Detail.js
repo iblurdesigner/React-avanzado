1d16802d387931cdfa36d10f5e3733d4ed587a19
iblurdesigner <nevomen@gmail.com>
2020-03-24 21:08:27 -0500

    Performance con React Memo

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,11 +2,7 @@ import React from 'react'
 import { PhotoCardWithQuery } from '../container/PhotoCardWithQuery'
 import { Layout } from '../components/Layout'
 
-<<<<<<< HEAD
-export default ({ detailId }) => (
-=======
 export const Detail = ({ detailId }) => (
->>>>>>> hotfix2
   <Layout title={`Fotografía ${detailId}`}>
     <PhotoCardWithQuery id={detailId} />
   </Layout>
