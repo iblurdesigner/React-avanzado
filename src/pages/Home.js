035325143c21fca58154b7c72a0c564f3ffc5fdc
iblurdesigner <nevomen@gmail.com>
2020-03-24 20:48:17 -0500

    Performance con React Memo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 import { Layout } from '../components/Layout'
 
-export const Home = ({ id }) => {
+const HomePage = ({ id }) => {
   return (
     <Layout title='Tu app de fotos de mascotas' subtitle='Con Petgram puedes encontrar fotos de animales domésticos muy bonitos'>
       <title>Petgram - </title>
@@ -13,3 +13,7 @@ export const Home = ({ id }) => {
     </Layout>
   )
 }
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.id === props.id
+})
